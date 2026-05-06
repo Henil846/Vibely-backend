7e2a3b6ac8067e9767f32af8e906d8e4e0fd091e
Henil <hdpatel846@gmail.com>
2026-05-06 20:23:13 +0530

    feat: add Vercel deployment config

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const connectDB = require("./src/db/db");
 
 const PORT = process.env.PORT || 3000;
 
+// Connect to database
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
+
+// Export for Vercel serverless
+module.exports = app;
